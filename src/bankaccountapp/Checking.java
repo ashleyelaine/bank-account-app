5cea816bd6071d779d5076745f29aa43ee9f9467
Ashley Wright <ashleyelainewright@gmail.com>
2021-09-17 22:01:05 -0500

    Initial commit

---
+++ src/bankaccountapp/Checking.java
@@ -0,0 +1,34 @@
+package bankaccountapp;
+
+public class Checking extends Account {
+	// List the properties specific to a checking account
+	private int debitCardNumber;
+	private int debitCardPin;
+	
+	// Constructor to initialize checking account properties
+	public Checking(String name, String sSN, double initDeposit) {
+		super(name, sSN, initDeposit);
+		accountNumber = "2" + accountNumber;
+		setDebitCard();
+	}
+	
+	@Override
+	public void setRate() {
+		rate = getBaseRate() * .15;
+	}
+	
+	// List any methods specific to the checking account
+	private void setDebitCard() {
+		debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+		debitCardPin = (int) (Math.random() * Math.pow(10, 4));
+	}
+	
+	public void showInfo() {
+		super.showInfo();
+		System.out.println(
+				"Your Checking Account Features" +
+				"\n Debit Card Number: " + debitCardNumber +
+				"\n Debit Card Pin: " + debitCardPin
+				);
+	}
+}
